25c4a062c49aa04c36b1c688cb7e3ae627d4e3cb
gowtamkumar <gowtampaul0@gmail.com>
2024-04-24 10:51:29 +0600

    merge sort

---
+++ dsa/dsa-2024/merge-sort.js
@@ -0,0 +1,35 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      console.log(left[leftIndex]);
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+const arr = [-2, 1, 8, 5, 6, 2, 6, 8, 9];
+const sortedArr = mergeSort(arr);
+
+console.log(sortedArr);
