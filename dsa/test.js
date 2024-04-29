3d7cbaff621ee4453279e1df2904819372cf05c9
gowtamkumar <gowtampaul0@gmail.com>
2024-04-29 14:56:37 +0600

    set data stracture

--- dsa/test.js
+++ dsa/test.js
@@ -2,13 +2,14 @@
 //   setTimeout(function() { alert(i); }, 1000 + i);
 // }
 
-(function () {
-  var a = (b = 5);
-})();
+// (function () {
+//   var a = (b = 5);
+// })();
 
 // console.log(0.1+0.2 === 0.3);
 // console.log(NaN === NaNs);
 //  What is the purpose of TypeScript's "never" type?*
-const obj = { x: 1, y: 2, z: 3 };
-const { x, ...rest } = obj;
-console.log(x, rest);
+// const obj = { x: 1, y: 2, z: 3 };
+// const { x, ...rest } = obj;
+// console.log(x, rest);
+
