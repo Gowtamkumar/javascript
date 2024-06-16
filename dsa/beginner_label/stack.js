6db74d810478a0a10418bd8c8193846679555227
gowtamkumar <gowtampaul0@gmail.com>
2024-06-16 10:17:12 +0600

    stack

--- dsa/beginner_label/stack.js
+++ dsa/beginner_label/stack.js
@@ -1,58 +1,58 @@
-// // stack is (LIFO)-> last-in first out
-// // call stack in javascript runtime
-// class Stack {
-//   constructor() {
-//     this.stack = [];
-//     this.size = 0;
-//     this.stackLen = 5;
-//   }
+// stack is (LIFO)-> last-in first out
+// call stack in javascript runtime
+class Stack {
+  constructor() {
+    this.stack = [];
+    this.size = 0;
+    this.stackLen = 5;
+  }
 
-//   isEmpty() {
-//     if (this.size === 0) {
-//       console.log("your stack is empty!");
-//     }
-//   }
+  isEmpty() {
+    if (this.size === 0) {
+      console.log("your stack is empty!");
+    }
+  }
 
-//   peek() {
-//     if (this.isEmpty()) {
-//       this.isEmpty();
-//     } else {
-//       console.log("get fron of the stack", this.stack[this.size - 1]);
-//     }
-//   }
+  peek() {
+    if (this.isEmpty()) {
+      this.isEmpty();
+    } else {
+      console.log("get fron of the stack", this.stack[this.size - 1]);
+    }
+  }
 
-//   push(value) {
-//     if (this.size < this.stackLen) {
-//       this.stack[this.size] = value;
-//       this.size += 1;
-//     } else {
-//       console.log("Your stack is Full!");
-//     }
-//   }
+  push(value) {
+    if (this.size < this.stackLen) {
+      this.stack[this.size] = value;
+      this.size += 1;
+    } else {
+      console.log("Your stack is Full!");
+    }
+  }
 
-//   pop() {
-//     if (this.size > 0) {
-//       this.size -= 1;
-//       delete this.stack[this.size];
-//     } else {
-//       console.log("Your stack is Empty!");
-//     }
-//   }
-// }
+  pop() {
+    if (this.size > 0) {
+      this.size -= 1;
+      delete this.stack[this.size];
+    } else {
+      console.log("Your stack is Empty!");
+    }
+  }
+}
 
-// const res = new Stack();
-// res.push(20);
-// res.push(5);
-// res.push(8);
-// res.push(7);
+const res = new Stack();
+res.push(20);
+res.push(5);
+res.push(8);
+res.push(7);
+res.push(6);
+res.peek();
 // res.push(6);
-// res.peek();
-// // res.push(6);
+res.pop();
+res.peek();
 // res.pop();
-// res.peek();
-// // res.pop();
-// // res.pop();
-// // res.pop();
-// // res.pop();
-// // res.pop();
-// console.log("🚀 ~ res:", res);
+// res.pop();
+// res.pop();
+// res.pop();
+// res.pop();
+console.log("🚀 ~ res:", res);
