c3ceb20c22bd6e8c37f1927faa000645aa9545a9
gowtamkumar <gowtampaul0@gmail.com>
2024-08-31 07:39:25 +0600

    heap

--- dsa/intermeidate_label/heap/heap.js
+++
@@ -1,4 +0,0 @@
-// jodi kono tree te children then parent boro hoy tahole tahole Heap bole.
-// jodi parent boro na hoy tahole seta ke heap bole na
-
-
---
+++ dsa/intermeidate_label/heap/heap1.js
@@ -0,0 +1,7 @@
+// 1.max
+// 2.min
+// jodi kono tree te children then parent boro hoy tahole tahole max Heap bole.
+// jodi parent boro na hoy tahole seta ke min heap bole
+
+
+ 
