861706b8a41bd5331d199b60cd969e570953c75c
gowtamkumar <gowtampaul0@gmail.com>
2024-09-17 12:54:31 +0600

    object and boolean tesing

--- testing/jest-with-js/sum.test.js
+++ testing/jest-with-js/sum.test.js
@@ -1,5 +1,31 @@
-const sum  = require("./sum")
+const sum = require("./sum");
 
-test("adds 1 +2 sum is 3", ()=> {
-  expect(sum(1, 2)).toBe(3)
+test("adds 1 +2 sum is 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+// this testing without fun
+test("two plus two is four", () => {
+  expect(2 + 2).toBe(4);
+});
+
+// object testing
+
+test("object assigment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+
+// boolean testing
+
+test("null is falsy", ()=> {
+  const n = 0 // 0/ false;
+  expect(n).toBeFalsy()
+})
+
+test("null is truthy", ()=> {
+  const n = 1 //1, true;
+  expect(n).toBeTruthy()
 })
